81938168ccba0038ac2fedfb44b6593fa7fadcc1
carlos benavides <1992cab@gmail.com>
2021-07-16 14:39:13 -0500

    chore: initial config project

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// port
+process.env.PORT = process.env.PORT || 3000;
+
+// database
+process.env.DB = 'mongodb+srv://carlosbean:5wa7XGD5sH454Rkx@cluster0-jsfn9.mongodb.net/hotel';
---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('./config/config')
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require("cors");
+
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//cors
+app.use(
+    cors({
+        origin: true,
+        credentials: true
+    })
+);
+
+mongoose.connect(process.env.DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Database online');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+});
